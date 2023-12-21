07dd4577b3d90698966b8036642f4605376f8b27
LauDic <lautarodichio@hotmail.com>
2023-12-21 17:05:36 -0300

    FirstSoundBank

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,87 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID BALL_JUMPIMPACT = 2556507283U;
+        static const AkUniqueID BALL_MOVEMENT = 285418122U;
+        static const AkUniqueID BALL_WATER = 3822983202U;
+        static const AkUniqueID DIAMOND_AIM = 1050447289U;
+        static const AkUniqueID DIAMOND_CATCH = 1460941491U;
+        static const AkUniqueID FASTZONE = 2619332785U;
+        static const AkUniqueID FINISH = 2555741448U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID TEST1_01 = 3773325688U;
+        static const AkUniqueID TEST1_02 = 3773325691U;
+        static const AkUniqueID TEST1_03 = 3773325690U;
+        static const AkUniqueID TEST1_04 = 3773325693U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace TERRAIN
+        {
+            static const AkUniqueID GROUP = 354267144U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DUST = 2348606633U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID METAL = 2473969246U;
+                static const AkUniqueID WATER = 2654748154U;
+            } // namespace SWITCH
+        } // namespace TERRAIN
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID BALLVELOCITY = 149143475U;
+        static const AkUniqueID VICTORYLANE = 1267329435U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANK_01 = 3200950630U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AUDIOOBJECT = 2524980176U;
+        static const AkUniqueID ENVIRONMENT = 1229948536U;
+        static const AkUniqueID MAINMIX = 2782633896U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID OTHERS = 3042021448U;
+        static const AkUniqueID PASSTHROUGH = 2804736953U;
+        static const AkUniqueID PLAYER = 1069431850U;
+        static const AkUniqueID REVAUX = 29600586U;
+        static const AkUniqueID UI = 1551306167U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REVERB = 348963605U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
